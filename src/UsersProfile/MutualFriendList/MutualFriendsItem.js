977dcc910febf3f4f55f33636791b099c511fff8
korivirendra <it.a.54.virendra.kori@gmail.com>
2023-10-15 12:33:34 +0530

    Updated Users profile

--- src/UsersProfile/MutualFriendList/MutualFriendsItem.js
+++ src/UsersProfile/MutualFriendList/MutualFriendsItem.js
@@ -1,5 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { withBrowseUser } from "../../../Components/HOC/withBrowseUser";
-
 const MutualFriendsItem = (props) => <small title={props.item?.receiverName} onClick={(x)=> {props.browseUser(props.item?.receiverUid, props.item?.receiverName); x.stopPropagation()}} className="similar__followers__item"> {props.item?.receiverName}</small>
 export default withBrowseUser(MutualFriendsItem);
